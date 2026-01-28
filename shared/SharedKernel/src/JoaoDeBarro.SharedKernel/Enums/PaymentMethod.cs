fe6e50935962e91f1d9a0e707569480bd35bfa76
xpimenta <matheuspr96@hotmail.com>
2026-01-28 12:34:52 +0000

    Adding SharedKernel Project

---
+++ shared/SharedKernel/src/JoaoDeBarro.SharedKernel/Enums/PaymentMethod.cs
@@ -0,0 +1,14 @@
+namespace JoaoDeBarro.SharedKernel.Enums;
+
+
+/// <summary>
+/// Payment Method (Forma de Pagamento)
+/// </summary>
+public enum PaymentMethod
+{
+    Boleto = 1,
+    Debit,
+    Credit,
+    Deposit,
+    Pix
+}
